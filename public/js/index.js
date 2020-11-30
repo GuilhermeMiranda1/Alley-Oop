ba5f47d66cd82ce0388bcc214f1987d657fffabb
GuilhermeMiranda1 <guilherme.miranda@bantec.com.br>
2020-11-30 13:25:56 -0300

    +basquete in progress

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+function telaInfo(){
+    window.location.href = "info.html"
+};
+
+function telaMaisBasquete(){
+    window.location.href = "maisbasquete.html"
+};
+
+function telaDraft(){
+    window.location.href = "draft.html"
+}
--- public/js/maisbasquete.js
+++ public/js/maisbasquete.js
@@ -1,12 +1,21 @@
-function descobrir() {
+function maisbasquete() {
     var infoUsuario = {
         "peso": pesoUsuario.value,
         "altura": alturaUsuario.value,
         "estilo": escolherEstilo.value,
         "tipo": tipoJogo.value
     };
-
-    if(infoUsuario.altura <= 175){
+    console.log(infoUsuario)
+    if (infoUsuario.altura <= 170 && infoUsuario.altura >= 140) {
+        document.getElementById("container2").style.display = "block";
         
+        mensagem.innerHTML = `<img src="img/lillard.png" alt="Damian Lillard" style="margin-top: -0.1%; height: 500px;;margin-left: 52%;">
+        <h2 style="margin-left:-20%; margin-top:-35%;font-size: 33px;text-align:center; text-shadow: 2px 2px #000000; -webkit-text-stroke-width: 2px;
+        -webkit-text-stroke-color: rgba(0, 0, 0, 0.131);; color: #FDB927;">Você tem o perfil de<br> <span
+                 style="color:red;">ARMADOR</span></h2> <br> <br>
+                 <p
+                style="margin-left:-20%;font-size:20px ;text-align: center; color: white;text-shadow: 2px 2px #552583; -webkit-text-stroke-width: 2px;-webkit-text-stroke-color:rgba(0, 0, 0, 0.131);">
+                Sua altura irá acabar te impedindo de executar outras  <br> funções de forma efetiva, portanto a posição <br> recomendada para você é a de ARMADOR </p>`
     }
+
 }
